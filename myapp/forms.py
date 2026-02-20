f0c533793e5a454e1f2a237fc713de2e7c9136c5
Ishan K <ishank@Ishans-MacBook-Air.local>
2026-02-20 22:17:26 +0530

    Updated forms and views for multi-select and API fields

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,15 +4,9 @@ from .models import Company
 class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
-        # This tells Django to include ALL fields from the model
-        fields = '__all__'
-        
+        fields = '__all__'  # This pulls EVERYTHING from models.py
         widgets = {
             'specialities': forms.SelectMultiple(attrs={'class': 'select2-multi'}),
-            # We change these to Select widgets so the JS can fill them with options
             'country': forms.Select(attrs={'id': 'country-select'}),
             'state': forms.Select(attrs={'id': 'state-select'}),
-            # You can also add classes to make text inputs look better
-            'name': forms.TextInput(attrs={'placeholder': 'Enter Company Name'}),
-            'ptc': forms.TextInput(attrs={'placeholder': 'Point of Contact Name'}),
         }
--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import *
+from .models import *
 
 def signup_view(request):
     if request.method == 'POST':
@@ -29,3 +30,23 @@ def add_company(request):
     else:
         form = CompanyForm()
     return render(request, 'add_company.html', {'form': form})
+
+
+def find_company(request):
+    services = Service.objects.all()
+    results = Company.objects.all()
+    
+    # Filter by name
+    name_query = request.GET.get('name')
+    if name_query:
+        results = results.filter(name__icontains=name_query)
+        
+    # Filter by multiple specialities
+    selected_services = request.GET.getlist('specialities')
+    if selected_services:
+        results = results.filter(specialities__id__in=selected_services).distinct()
+
+    return render(request, 'find_company.html', {
+        'results': results, 
+        'all_services': services
+    })
